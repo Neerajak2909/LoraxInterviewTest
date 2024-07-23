2b915f37b91997864879e967cebfb1edbc4b3655
Neerajak2909 <neerajak29@gmail.com>
2024-07-23 09:51:06 +0100

    Updated

--- InterviewTestQA/InterviewTestQA/InterviewTestAutomation/JSONTest.cs
+++ InterviewTestQA/InterviewTestQA/InterviewTestAutomation/JSONTest.cs
@@ -4,23 +4,57 @@ using Xunit;
 using System.Collections.Generic;
 using System.IO;
 using Xunit;
+using System.Xml.Linq;
+using System.Collections;
+using Xunit.Abstractions;
+using ThirdParty.Json.LitJson;
+using System.Transactions;
 
 namespace InterviewTestQA
 {
     public class JSONTest
     {
+        private readonly ITestOutputHelper output;
+
+        public JSONTest(ITestOutputHelper output)
+        {
+            this.output = output;
+        }
+
         [Fact]
         public void TestDeserialization()
         {
             // Read the Json file
-            string jsonFilePath = Path.Combine(Directory.GetCurrentDirectory(), "InterviewTestAutomation/Data", "Cost Analysis.json");
+            string jsonFilePath = Path.Combine(Directory.GetCurrentDirectory(), "InterviewTestAutomation", "Data", "Cost Analysis.json");
             string json = File.ReadAllText(jsonFilePath);
 
-            // Deserialize the Json into a list of Cost Analysis objects
+            // Deserialize the Json into a variable containing list of Json Objects
             var costAnalysisList = JsonConvert.DeserializeObject<List<CostAnalysis>>(json);
 
             // Assert the number of items in the list
-            Assert.Equal(53, costAnalysisList.Count);
+            int expectedCount = 53;
+            Assert.Equal(expectedCount, costAnalysisList.Count);
+            output.WriteLine($"Total Number of items in the list: {costAnalysisList.Count}");
+            
+            // Fetch the top item for  from the list and sort Cost in descending order
+            var topItem = costAnalysisList.OrderByDescending(item => item.Cost).FirstOrDefault();
+
+            // Assert the top item exists
+            Assert.NotNull(topItem);
+            output.WriteLine($"Top Item in the list which is maximum cost : {topItem.Cost}");
+
+            // Assert the Country ID of the top item
+            int expectedCountryId = 0;
+            Assert.Equal(expectedCountryId, topItem.CountryId);
+            output.WriteLine($"Top Country in the list: {topItem.CountryId}");
+            
+            // Calculate Sum of cost for the Year 2016
+            var sumOfCost = costAnalysisList.Where(item => item.YearId.Equals("2016")).Sum(item  => item.Cost);
+
+            // Assert the Total Sum of cost for Year 2016
+            double expectedSumOfCost = 77911.374456100006;
+            Assert.Equal(expectedSumOfCost, sumOfCost);
+            output.WriteLine($"Total Sum of Costs for Year ID 2016 is : {sumOfCost}");
         }
     }
 }
--- InterviewTestQA/InterviewTestQA/obj/Debug/net6.0/InterviewTestQA.AssemblyInfo.cs
+++ InterviewTestQA/InterviewTestQA/obj/Debug/net6.0/InterviewTestQA.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InterviewTestQA")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af2d4b648c21738e7428b0ed73a54eb34fda9f55")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7452c6637d24e1352717c21209e5e67cfd127028")]
 [assembly: System.Reflection.AssemblyProductAttribute("InterviewTestQA")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InterviewTestQA")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
